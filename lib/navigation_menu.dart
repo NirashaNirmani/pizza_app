8e2fb29d54bee43d6e4757053cb05cdc6ccdca84
NirashaNirmani <nishinirmani@gmail.com>
2024-01-20 14:57:07 +0530

    Dependencies added Navigationbar created

---
+++ lib/navigation_menu.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    //final darkMode = EHelperFunctions.isDarkMode(context);
+    return Scaffold(
+        bottomNavigationBar: Obx(() => NavigationBar(
+                height: 80,
+                elevation: 0,
+                backgroundColor: Color.fromARGB(244, 0, 13, 84),
+                indicatorColor: Colors.white.withOpacity(0),
+                selectedIndex: controller.selectedIndex.value,
+                onDestinationSelected: (index) =>
+                    controller.selectedIndex.value = index,
+                destinations: const [
+                  NavigationDestination(
+                    icon: Icon(Iconsax.home, color: Colors.white),
+                    label: "Home",
+                  ),
+                  NavigationDestination(
+                      icon: Icon(Iconsax.shop, color: Colors.white),
+                      label: "Shop"),
+                  NavigationDestination(
+                      icon: Icon(Iconsax.heart, color: Colors.white),
+                      label: "Wishlist"),
+                  NavigationDestination(
+                      icon: Icon(Iconsax.user, color: Colors.white),
+                      label: "Profile"),
+                ])),
+        body: Obx(
+          () => controller.screens[controller.selectedIndex.value],
+        ));
+  }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    Container(
+      color: Colors.amberAccent,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: const Color.fromARGB(255, 48, 46, 38),
+    ),
+    Container(
+      color: Colors.pink,
+    ),
+    // const StoreScreen(),
+    // const Favouritescreen(),
+    // const SettingsScreen(),
+  ];
+}
